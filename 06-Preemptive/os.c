e2b4b3817ee79f3dc3d7ed501e6edf63b41ebd3d
Neal <hunter0hunter04@hotmail.com.tw>
2015-04-18 16:37:19 +0800

    Seperate helper function into 'helper.c' file.

--- 06-Preemptive/os.c
+++ 06-Preemptive/os.c
@@ -3,6 +3,7 @@
 #include "reg.h"
 #include "asm.h"
 #include "host.h"
+#include "helper.c"
 
 /* Size of our user task stacks in words */
 #define STACK_SIZE	256
@@ -150,81 +151,6 @@ unsigned int get_time(){
 	return tickcount * scale +
            ((float)*SYSTICK_LOAD - (float)*SYSTICK_VAL) / ((float)*SYSTICK_LOAD / scale);
 }
-
-int _snprintf_int(int num, char *buf, int buf_size)
-{
-	int len = 1;
-	char *p;
-	int i = num < 0 ? -num : num;
-
-	for (; i >= 10; i /= 10, len++);
-
-	if (num < 0)
-		len++;
-
-	i = num;
-	p = buf + len - 1;
-	do {
-		if (p < buf + buf_size)
-			*p-- = '0' + i % 10;
-		i /= 10;
-	} while (i != 0);
-
-	if (num < 0)
-		*p = '-';
-
-	return len < buf_size ? len : buf_size;
-}
-
-int snprintf(char *buf, size_t size, const char *format, ...)
-{
-	va_list ap;
-	char *dest = buf;
-	char *last = buf + size;
-	char ch;
-
-	va_start(ap, format);
-	for (ch = *format++; dest < last && ch; ch = *format++) {
-		if (ch == '%') {
-			ch = *format++;
-			switch (ch) {
-			case 's' : {
-					char *str = va_arg(ap, char*);
-					/* strncpy */
-					while (dest < last) {
-						if ((*dest = *str++))
-							dest++;
-						else
-							break;
-					}
-				}
-				break;
-			case 'd' : {
-					int num = va_arg(ap, int);
-					dest += _snprintf_int(num, dest,
-					                      last - dest);
-				}
-				break;
-			case '%' :
-				*dest++ = ch;
-				break;
-			default :
-				return -1;
-			}
-		} else {
-			*dest++ = ch;
-		}
-	}
-	va_end(ap);
-
-	if (dest < last)
-		*dest = 0;
-	else
-		*--dest = 0;
-
-	return dest - buf;
-}
-
 void task_switch_time(){
 	
 //	char *s = "switch\n";
